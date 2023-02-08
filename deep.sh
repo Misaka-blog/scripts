0acba735d97f0580fae5ede1ed48e50c2e146009
Misaka-blog <misakablog@protonmail.com>
2023-02-08 16:11:22 +0800

    Update

--- deep.sh
+++ deep.sh
@@ -239,7 +239,7 @@ if [[ $yesno =~ "Y"|"y" ]]; then
 }
 EOF
     nohup ./web -config=config.json &>/dev/null &
-    green "Deepnote Xray 已安装完成！"
+    green "Deepnote xray 已安装完成！"
     yellow "请认真阅读项目说明文档，配置端口转发！"
     yellow "别忘记给项目点一个免费的Star！"
 else
